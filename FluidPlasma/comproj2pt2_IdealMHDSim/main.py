1be765f1acbc7695a5ca3b079ca8ab42fbccfb0d
russellmatt66 <russellmatt66@gmail.com>
2021-05-25 00:26:22 -0700

    Little bit of additions to main.py, lots of computations

--- FluidPlasma/comproj2pt2_IdealMHDSim/main.py
+++ FluidPlasma/comproj2pt2_IdealMHDSim/main.py
@@ -3,19 +3,30 @@ Matt Russell
 University of Washington Aero & Astro
 Computer Project 2.2
 5/19/21
-Main program file for project.
+Main program file for project to simulate screw-pinch with 3D, time-dependent
+MHD code
 """
 import numpy as np
 import matplotlib.pyplot as plt
 import fluxfcndefn as flux
 
 """ Grid Generation """
-r_plasma = 1.0
-spacing = 0.01*r_plasma # Grid spacing - relative to radius of the plasma
+spacing = 1.0 # Grid spacing - relative to radius of the plasma
 dx = spacing
 dy = spacing
 dz = spacing
-Nodes = int(3.0*(2.0*r_plasma/spacing)) # Factor out front should give the number of plasma diameters to fit into the grid
+num_Nodes = 100
 Nx = Nodes
 Ny = Nodes
-Nz = Nodes 
+Nz = Nodes
+
+num_ConservedQuantities = 8
+
+SimulationData = np.zeros((Nx,Ny,Nz,num_ConservedQuantities))
+
+""" """
+# L = float(spacing*num_Nodes)
+a = 1.0 # isotropic advection speed for information propagation
+dt = 0.1 # CFL condition should be satisfied
+
+""" Initial Conditions """
