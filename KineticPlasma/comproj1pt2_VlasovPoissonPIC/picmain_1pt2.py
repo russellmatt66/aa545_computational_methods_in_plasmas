There is something wrong with the electric field calculation - remember Uri's words about there being an issue with my boundary condition

KineticPlasma/comproj1pt2_VlasovPoissonPIC/picmain_1pt2.py
@@ -53,7 +53,8 @@ FDmtx = pmod.FirstDerivativeStencil(Nx,dx)
 print("Closing Grid Generation Phase")
 
 print("Initializaing Particle Distribution")
-# 
+a = (L - 2.0*dx)/(N-1) # linear coefficients for evenly distributing particles
+b = x_min + dx 
 if (N == 2 and InitialV == 0):
     x_i[0] = -np.pi/4.0
     x_i[1] = np.pi/4.0
@@ -62,7 +63,7 @@ if (N == 2 and InitialV == 0):
 
 if(N != 2 and InitialV == 0):
     for pidx in np.arange(N):
-        x_i[pidx] = (x_min + dx) + float(pidx)*2.0*(x_max + dx)/float(N-1)
+        x_i[pidx] = a*pidx + b
         v_i[pidx] = 0.0
 
 if(N == 2 and InitialV == 1):
@@ -75,7 +76,7 @@ if(N == 2 and InitialV == 1):
 if(N != 2 and InitialV == 1):
     vprime = 0.001 * v_th
     for pidx in np.arange(N):
-        x_i[pidx] = (x_min + dx) + float(pidx)*2.0*(x_max + dx)/float(N-1)
+        x_i[pidx] = a*pidx + b
         v_i[pidx] = vprime*np.sin(2.0*np.pi/L * x_i[pidx])
 
 """ Initialize Diagnostics and Simulation Parameters """
@@ -84,16 +85,17 @@ PhaseSpaceFig = plt.figure()
 FieldFig = plt.figure()
 ChargeDensityFig = plt.figure()
 
-dt = 0.032 * tau_p
-Nt = 100
+dt = 0.005 * tau_p
+Nt = 500
 
 KineticEnergy = np.zeros(Nt)
 ElectricFieldEnergy = np.zeros(Nt)
 TotalEnergy = np.zeros(Nt)
 
+
 """ Main Loop """
 for n in np.arange(Nt):
-    print("Taking step %i" %n)
+    print("Taking step %i" %(n+1))
     KineticEnergy[n] = pmod.ComputeKineticEnergy(v_i,m_sp) # Compute before particle push
     # Plot particle locations in phase-space
     plt.figure(PhaseSpaceFig.number)
@@ -111,8 +113,8 @@ for n in np.arange(Nt):
         plt.title('Initial grid charge density, N = %i particles' %N)
         plt.figure(FieldFig.number)
         plt.plot(x_grid,E_j)
-        plt.title('Initial grid electric field, N = %i particles' %N)
-
+        plt.title('Initial grid electric field, N = %i particles')
+                  
 # Plot Energy 
 tvector = np.linspace(0.0,float((Nt-1)*dt),Nt)
 plt.figure(EnergyFig.number)

KineticPlasma/comproj1pt2_VlasovPoissonPIC/picmodule_1pt2.py
@@ -167,7 +167,7 @@ def LaplacianStencil(Nx,dx,eps_0):
     vals = np.vstack((v,-2.0*v,v))
     Lmtx = sp.spdiags(vals,diags,Nx,Nx);
     Lmtx = sp.lil_matrix(Lmtx); # need to alter entries
-    Lmtx[0,Nx-1] = -1.0
+    Lmtx[0,Nx-1] = 1.0
     LaplPreFactor = eps_0 * dx**2
     Lmtx /= LaplPreFactor
     Lmtx = sp.csr_matrix(Lmtx)
@@ -288,9 +288,9 @@ def LeapFrog(x_i,v_i,E_i,dt,qm,n,X_min,X_max):
     """
     if n == 0: # First step requires an initial velocity half-step to begin
         v_i = EulerStep(-dt/2.0,E_i,v_i,qm)# half-step backwards gives v_i(t_{-1/2}) to begin
-        # x_i = x_i + dt*v_i - Artifact of Forward Euler half=step
     v_i = v_i + qm*dt*E_i # v_i(t_{n+1/2}) = v_i(t_{n-1/2}) + (q/m)*dt*E_i
     x_i = x_i + dt*v_i # x_i(t_{n+1}) = x_i(t_{n}) + dt*v_i(t_{n+1/2})
+    """
     for pidx in np.arange(np.size(x_i)):
         if(x_i[pidx] > X_max): # particle exited grid via right boundary
             Dx = x_i[pidx] - X_max
@@ -298,6 +298,7 @@ def LeapFrog(x_i,v_i,E_i,dt,qm,n,X_min,X_max):
         if(x_i[pidx] < X_min):# particle exited grid via left boundary
             Dx = x_i[pidx] - X_min
             x_i[pidx] = X_max + Dx
+    """
     return x_i, v_i
 
 """ Diagnostics """

KineticPlasma/comproj1pt2_VlasovPoissonPIC/test.py
@@ -10,15 +10,16 @@ import picmodule_1pt2 as pmod
 import numpy as np
 import matplotlib.pyplot as plt
 
-Nx = 33
+Nx = 33 # number of grid points = 32 + 1
 x_min = -np.pi
 x_max = np.pi
 L = x_max - x_min
 x_grid = np.linspace(x_min,x_max,Nx,dtype=float)
 dx = (x_max - x_min)/float(Nx)
-N = 4
+N = 4 # number of particles
 x_i = np.zeros((N,1),dtype=float)
 v_i = np.zeros((N,1),dtype=float)
+WeightingOrder = 1
 
 # Normalization
 omega_p = 1.0
@@ -31,7 +32,26 @@ Te = 1.0 # [eV]
 lambda_De = np.sqrt(Te * N * (q_over_m**2) / (eps_0 * L))
 v_th = omega_p * lambda_De
 
+""" Examine Details of Particle-Weighting """
+ChargeDensityFig = plt.figure()
+N = 4 # Keep things local
+WeightingOrder = 1
+rho_j = np.zeros((Nx,1),dtype=float)
+a = (L - 2.0*dx)/(N-1) # linear coefficients for evenly distributing particles
+b = x_min + dx
+for pidx in np.arange(N):
+    x_i[pidx] = a*pidx + b
+    print("The location of particle %i is %3.2f" %(pidx+1,x_i[pidx]))
+
+rho_j = pmod.ParticleWeighting(WeightingOrder,x_i,N,x_grid,Nx,dx,L,rho_j,q_sp)
+plt.figure(ChargeDensityFig.number)
+plt.plot(x_grid,rho_j)
+plt.scatter(x_i,np.ones(np.size(x_i)))
+plt.axvline(x=x_min)
+plt.axvline(x=x_max)
+
 """ Test Findj/B.Cs """
+"""
 # Conclusion: Findj works correctly for the most part
 # Exception: When x_i is located exactly on a grid point
 # xfind = x_max - 0.5*dx
@@ -41,6 +61,7 @@ for xidx in np.arange(np.size(x_to_find)):
     jfound = pmod.Findj(x_grid,x_to_find[xidx])
     jfoundp1 = (jfound +1) % Nx
     print("For x_i = %f, jfound is %i and jfoundp1 is %i" %(x_to_find[xidx],jfound,jfoundp1))
+"""
 
 """ Test particle distribution Initialization for N = 64 """
 """