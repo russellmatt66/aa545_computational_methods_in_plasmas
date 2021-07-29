5e64ad7d57a7eadb87b2cdfa0bdd12153f6bbd41
russellmatt66 <russellmatt66@gmail.com>
2021-07-28 22:23:21 -0700

    Search algorithm actually seems to be working correctly

--- KineticPlasma/comproj1pt2_VlasovPoissonPIC/test_PW.py
+++ KineticPlasma/comproj1pt2_VlasovPoissonPIC/test_PW.py
@@ -65,6 +65,27 @@ def PWmove(x_i,v_i,dt,rho_j,flag,iter): # Watch a single particle move through t
     iter = iter + 1
     return PWmove(x_i,v_i,dt,rho_j,flag,iter)
 
+def PWmoveplusfind(x_i,v_i,dt,rho_j,flag,iter): # Watch a single particle move through the grid
+    print('This is iteration %i' %iter)
+    # flag = 0 is standard
+    if flag == 1: # particle reached other side of grid
+        return 0
+    plt.scatter(x_i,np.ones(np.size(x_i)),label='Particles')
+    jfound = pmod.Findj(x_grid,x_i)
+    jfoundp1 = (jfound + 1) % Nx
+    plt.axvline(x=x_grid[jfound],ls='--')
+    plt.axvline(x=x_grid[jfoundp1],ls='--')
+    rho_j = pmod.ParticleWeighting(1,x_i,x_grid,Nx,dx,L,rho_j,q_sp) # WeightOrd=1
+    plt.scatter(x_grid,rho_j,label='Charge Density')
+    plt.title('Charge density from a single particle moving through the grid')
+    plt.legend()
+    plt.show()
+    x_i[0] = x_i[0] + v_i*dt
+    if np.abs(x_i[0]) > x_max: # particle motion under control but this handles left
+        flag = 1
+    iter = iter + 1
+    return PWmoveplusfind(x_i,v_i,dt,rho_j,flag,iter)
+
 """ Step through N \in [2,64] 
 Nmin = 2
 Nmax = 64
@@ -89,4 +110,39 @@ q_sp = (eps_0 * L / N) * (1.0 / q_over_m)
 flag = 0
 plt.figure()
 rvar = PWmove(x_i,v_i,dt,rho_j,flag,0)
+"""
+
+""" Watch single particle move through grid and track search algorithm results
+# Conclusion: search algorithm appears to be working correctly based on observations
+x_i = -np.pi
+Nt = 100
+dt = 1.0
+v_i = L/(float(Nt)*dt)
+
+for tidx in np.arange(Nt):
+    jfound = pmod.Findj(x_grid,x_i)
+    jfoundp1 = (jfound + 1) % Nx
+    plt.axvline(x=x_grid[jfound])
+    plt.axvline(x=x_grid[jfoundp1])
+    plt.scatter(x_i,np.ones(1),label='Particle')
+    plt.scatter(x_grid,np.zeros(Nx),label='Grid Points')
+    plt.title('Search algorithm output')
+    plt.legend()
+    plt.show()
+    x_i = x_i + v_i*dt
+"""
+
+""" Watch single particle move through grid, track search algo results, and charge density weighting 
+# Conclusion: Honestly, everything here seems to be hunky-dory
+N = 1
+x_i = np.zeros(N)
+x_i[0] = -np.pi
+Nt = 100
+dt = 1.0
+v_i = L/(float(Nt)*dt)
+rho_j = np.zeros((Nx,1),dtype=float)
+q_sp = (eps_0 * L / N) * (1.0 / q_over_m)
+flag = 0
+plt.figure()
+rvar = PWmoveplusfind(x_i,v_i,dt,rho_j,flag,0)
 """
