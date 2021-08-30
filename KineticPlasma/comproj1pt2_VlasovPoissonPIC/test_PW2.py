7dc15eab11ea837e3e11bea07807816f02c38004
russellmatt66 <russellmatt66@gmail.com>
2021-08-30 16:36:21 -0700

    Progress made on visualization, next step is to plot rho instead of scatter

--- KineticPlasma/comproj1pt2_VlasovPoissonPIC/test_PW2.py
+++ KineticPlasma/comproj1pt2_VlasovPoissonPIC/test_PW2.py
@@ -16,16 +16,24 @@ Np_vec = np.array([Nprtcl for Nprtcl in range(N_min,N_max+1)])
 #rho_j = pmod.RHO_j
 #x_grid = pmod.X_GRID
 
-ChrgFig = plt.figure()
-ChrgAx = plt.axes()
-ChrgPlot, = ChrgAx.plot([], [], lw=3)
-scatter_rho = plt.scatter(pmod.X_GRID,pmod.RHO_j)
+#ChrgFig = plt.figure()
+#ChrgAx = plt.axes()
+#plt.xlim(pmod.X_MIN,pmod.X_MAX)
+ChrgFig, ChrgAx = plt.subplots(1,2)
 
+sc_rho = ChrgAx[0].scatter([],[])
+ChrgAx[0].set_xlim(pmod.X_MIN,pmod.X_MAX)
+
+sc_prtcl = ChrgAx[1].scatter([],[])
+ChrgAx[1].set_xlim(pmod.X_MIN,pmod.X_MAX)
+
+"""
 def init():
     ChrgPlot.set_data([], [])
     ChrgAx.set_xlim(pmod.X_MIN,pmod.X_MAX)
     ChrgAx.set_ylim(-3.0,3.0)
     return ChrgPlot,
+"""
 
 def animate(N):
     # I need each frame to show the charge density and each particle for that particular N with NO superposition, i.e, for N = 20 I only want to see the data for 20 particles. I do not want data from N = 16, 17, etc. to also be on the figure. 
@@ -37,13 +45,12 @@ def animate(N):
     for pidx in np.arange(N):
         x_i[pidx] = a*pidx + b
     pmod.ParticleWeighting(1,x_i,q_sp) # updates rho_j
-    data = np.hstack((pmod.X_GRID, pmod.RHO_j))
-    scatter_rho.set_offsets(data)
-    #ChrgAx.scatter(pmod.X_GRID,pmod.RHO_j)
-    #ChrgPlot.set_data(pmod.X_GRID,pmod.RHO_j)
-    #ChrgPlot.set_data(x_i,np.ones(len(x_i)))
-    return ChrgPlot,
+    sc_rho.set_offsets(np.c_[pmod.X_GRID,pmod.RHO_j])
+    rhomax = np.amax(pmod.RHO_j)
+    ChrgAx[0].set_ylim(-rhomax,rhomax)
+    sc_prtcl.set_offsets(np.c_[x_i,np.ones(N)])
+    ChrgAx[1].set_ylim(0.0,2.0)
+    
+PrtclWgtAnimation = FuncAnimation(fig=ChrgFig, func=animate, frames=Np_vec, interval=1000, repeat=False)
 
-PrtclWgtAnimation = FuncAnimation(fig=ChrgFig, func=animate, frames=Np_vec, init_func=init, interval=250, blit=True, repeat=True)
-
-PrtclWgtAnimation.save('rho-ster_scan.gif', writer='imagemagick',fps=4)
+PrtclWgtAnimation.save('rho-ster_scan.gif', writer='imagemagick')
